1dbff158d1ad5d7fba65f3583e3cad5db5787568
Shao-Yu <shaoyu.ts@gmail.com>
2024-10-02 10:26:47 +0800

    feat: add both result in same figure

--- hw01_lorenz63.py
+++ hw01_lorenz63.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys, os
+import numba
 
 # Parameters
 sigma = 10
@@ -7,6 +9,7 @@ beta = 8 / 3
 rho = 24.74
 
 # Lorenz system equations
+## @numba.guvectorize([(float64[:], float64, float64, float64)], '(n),(),(),()->(n)')
 def lorenz_system(state, sigma, beta, rho):
     x, y, z = state
     dxdt = sigma * (y - x)
@@ -27,14 +30,20 @@ def rk4_step(state, dt, sigma, beta, rho):
     return state + (k1 + 2 * k2 + 2 * k3 + k4) / 6
 
 # Time settings
-dt_euler = 0.01  # Chosen time step for Euler method (small for stability)
-dt_rk4 = 0.05  # Larger time step for RK4 due to its accuracy
+dt_euler = 0.0000001  # Chosen time step for Euler method (small for stability)
+dt_rk4 = 0.0000001  # Larger time step for RK4 due to its accuracy
 t_max = 40
 num_steps_euler = int(t_max / dt_euler)
 num_steps_rk4 = int(t_max / dt_rk4)
 
 # Initial conditions
-initial_state = np.array([0.1, 0, 0])
+dum=np.sqrt(beta*(rho-1))
+initial_state = np.array([0, 0, 0], dtype=float)
+#initial_state = np.array([dum, dum, rho-1])
+#initial_state = np.array([-dum, -dum, rho-1])
+
+#add perturbation
+initial_state[0] = initial_state[0]+0.1
 
 # Running the Euler method
 states_euler = np.zeros((num_steps_euler, 3))
@@ -52,24 +61,38 @@ for i in range(1, num_steps_rk4):
 time_euler = np.linspace(0, t_max, num_steps_euler)
 time_rk4 = np.linspace(0, t_max, num_steps_rk4)
 
+# save results
+c = np.concatenate([time_euler[:,np.newaxis], states_euler], axis=1)
+np.savetxt(f'{dt_euler}_euler.txt',c,header='ts x y z')
+
+c = np.concatenate([time_rk4[:,np.newaxis], states_rk4], axis=1)
+np.savetxt(f'{dt_rk4}_rk4.txt',c,header='ts x y z')
+
+
+####################################
 # Plotting the results
-fig, axs = plt.subplots(3, 2, figsize=(12, 10))
+fig, axs = plt.subplots(3, 2, figsize=(12, 10), sharex=True)
+
+# find ylim for x, y, z
+ylim_min = np.vstack((np.min(states_euler, axis=0),\
+                      np.min(states_rk4, axis=0),\
+                    )).min(axis=0)
+ylim_max = np.vstack((np.max(states_euler, axis=0),\
+                      np.max(states_rk4, axis=0),\
+                    )).max(axis=0)
 
 # Euler method plots
-axs[0, 0].plot(time_euler, states_euler[:, 0])
-axs[0, 0].set_title('Euler Method: x vs time')
-axs[1, 0].plot(time_euler, states_euler[:, 1])
-axs[1, 0].set_title('Euler Method: y vs time')
-axs[2, 0].plot(time_euler, states_euler[:, 2])
-axs[2, 0].set_title('Euler Method: z vs time')
+axis_name=['x','y','z']
+for i in range(3):
+  axs[i, 0].plot(time_euler, states_euler[:, i])
+  axs[i, 0].set_title(f'Euler Method: {axis_name[i]} vs time')
+  axs[i, 0].set_ylim(ylim_min[i], ylim_max[i])
 
 # RK4 method plots
-axs[0, 1].plot(time_rk4, states_rk4[:, 0])
-axs[0, 1].set_title('RK4 Method: x vs time')
-axs[1, 1].plot(time_rk4, states_rk4[:, 1])
-axs[1, 1].set_title('RK4 Method: y vs time')
-axs[2, 1].plot(time_rk4, states_rk4[:, 2])
-axs[2, 1].set_title('RK4 Method: z vs time')
+for i in range(3):
+  axs[i, 1].plot(time_rk4, states_rk4[:, i])
+  axs[i, 1].set_title(f'RK4 Method: {axis_name[i]} vs time')
+  axs[i, 1].set_ylim(ylim_min[i], ylim_max[i])
 
 # Final adjustments
 for ax in axs.flat:
@@ -80,16 +103,46 @@ plt.tight_layout()
 plt.show()
 
 
+####################################
+# Plotting the results in same figure
+fig, axs = plt.subplots(3, 1, figsize=(6, 10), sharex=True)
+# find ylim for x, y, z
+ylim_min = np.vstack((np.min(states_euler, axis=0),\
+                      np.min(states_rk4, axis=0),\
+                    )).min(axis=0)
+ylim_max = np.vstack((np.max(states_euler, axis=0),\
+                      np.max(states_rk4, axis=0),\
+                    )).max(axis=0)
+# Euler method plots
+axis_name=['x','y','z']
+for i in range(3):
+  axs[i].plot(time_euler, states_euler[:, i], label='Euler')
+  axs[i].plot(time_rk4,   states_rk4[:, i], label='RK4')
+  axs[i].set_title(f'{axis_name[i]} vs time')
+  axs[i].set_ylim(ylim_min[i], ylim_max[i])
+  axs[i].legend()
+
+plt.show()
+
+
 # 3D Plot of the Lorenz system to show the butterfly effect
 
 fig = plt.figure(figsize=(10, 7))
 ax = fig.add_subplot(111, projection='3d')
 
 # Plot the trajectory from RK4 (since it's more accurate)
-ax.plot(states_rk4[:, 0], states_rk4[:, 1], states_rk4[:, 2], color='b')
+ax.plot(states_euler[:, 0], \
+        states_euler[:, 1], \
+        states_euler[:, 2], \
+        color='C0',label='Euler')
+ax.plot(states_rk4[:, 0], \
+        states_rk4[:, 1], \
+        states_rk4[:, 2], \
+        color='C1',label='RK4')
+plt.legend()
 
 # Labels and title
-ax.set_title('Butterfly Effect (Lorenz System) - RK4 Method')
+ax.set_title('Butterfly Effect (Lorenz System) - Euler/RK4 Method')
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
