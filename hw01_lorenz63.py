f822cfee509ac57badf6d85a3b31f8fd84f8b151
Shao-Yu <shaoyu.ts@gmail.com>
2024-10-02 17:06:38 +0800

    feat: add scipy solution

--- hw01_lorenz63.py
+++ hw01_lorenz63.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sys, os
-import numba
+from scipy.integrate import solve_ivp
 
 # Parameters
 sigma = 10
@@ -9,13 +9,13 @@ beta = 8 / 3
 rho = 24.74
 
 # Lorenz system equations
-## @numba.guvectorize([(float64[:], float64, float64, float64)], '(n),(),(),()->(n)')
 def lorenz_system(state, sigma, beta, rho):
     x, y, z = state
     dxdt = sigma * (y - x)
     dydt = x * (rho - z) - y
     dzdt = x * y - beta * z
-    return np.array([dxdt, dydt, dzdt])
+    output = np.array([dxdt, dydt, dzdt])
+    return output
 
 # Euler method
 def euler_step(state, dt, sigma, beta, rho):
@@ -30,11 +30,17 @@ def rk4_step(state, dt, sigma, beta, rho):
     return state + (k1 + 2 * k2 + 2 * k3 + k4) / 6
 
 # Time settings
-dt_euler = 0.0000001  # Chosen time step for Euler method (small for stability)
-dt_rk4 = 0.0000001  # Larger time step for RK4 due to its accuracy
+dt_euler = 0.00001  # Chosen time step for Euler method (small for stability)
+dt_rk4 = 0.00001  # Larger time step for RK4 due to its accuracy
+dt_sci = 0.00001  # Larger time step for RK4 due to its accuracy
 t_max = 40
 num_steps_euler = int(t_max / dt_euler)
 num_steps_rk4 = int(t_max / dt_rk4)
+num_steps_sci = int(t_max / dt_sci)
+# Time arrays
+time_euler = np.linspace(0, t_max, num_steps_euler)
+time_rk4 = np.linspace(0, t_max, num_steps_rk4)
+time_sci = np.linspace(0, t_max, num_steps_sci)
 
 # Initial conditions
 dum=np.sqrt(beta*(rho-1))
@@ -57,18 +63,20 @@ states_rk4[0] = initial_state
 for i in range(1, num_steps_rk4):
     states_rk4[i] = rk4_step(states_rk4[i - 1], dt_rk4, sigma, beta, rho)
 
-# Time arrays
-time_euler = np.linspace(0, t_max, num_steps_euler)
-time_rk4 = np.linspace(0, t_max, num_steps_rk4)
+# solve from scipy
+warpper_func = lambda t, y: lorenz_system(y, sigma, beta, rho)
+result_sci = solve_ivp(fun       = warpper_func, \
+                       t_span    = (0,t_max), \
+                       y0        = initial_state, \
+                       t_eval    = time_sci, \
+                      )
+states_sci = result_sci.y.T
+time_sci   = result_sci.t
 
-# save results
-c = np.concatenate([time_euler[:,np.newaxis], states_euler], axis=1)
-np.savetxt(f'{dt_euler}_euler.txt',c,header='ts x y z')
-
-c = np.concatenate([time_rk4[:,np.newaxis], states_rk4], axis=1)
-np.savetxt(f'{dt_rk4}_rk4.txt',c,header='ts x y z')
 
 
+
+sys.exit()
 ####################################
 # Plotting the results
 fig, axs = plt.subplots(3, 2, figsize=(12, 10), sharex=True)
@@ -116,6 +124,7 @@ ylim_max = np.vstack((np.max(states_euler, axis=0),\
 # Euler method plots
 axis_name=['x','y','z']
 for i in range(3):
+  axs[i].plot(time_sci, states_sci[:, i], label='Sci')
   axs[i].plot(time_euler, states_euler[:, i], label='Euler')
   axs[i].plot(time_rk4,   states_rk4[:, i], label='RK4')
   axs[i].set_title(f'{axis_name[i]} vs time')
